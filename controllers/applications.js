2ee45a1444ee3c07d65f31928b45247e3dd956b6
jason <jason_tan75@hotmail.com>
2024-06-25 21:29:02 -0400

    code up to 'build the new page'

---
+++ controllers/applications.js
@@ -0,0 +1,22 @@
+// controllers/applications.js
+
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+
+// we will build out our router logic here
+// controllers/applications.js
+
+router.get('/', async (req, res) => {
+    try {
+        res.render('applications/index.ejs');
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+    // (req, res) => {
+    // res.send('hello applications index route');}
+});
+
+module.exports = router;
